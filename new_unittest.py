9d82c5887dcea0f3984b42b0fbeb713e3d70ac7e
VincentLi1216 <sunnus.tw@gmail.com>
2024-02-23 23:36:13 +0800

    failed case

--- new_unittest.py
+++ new_unittest.py
@@ -9,10 +9,12 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.add(-1, -1), -2)
 
     def test_subtract(self):
-        self.assertEqual(calc.subtract(10, 5), 5)
+        self.assertEqual(calc.subtract(10, 5), 4)
         self.assertEqual(calc.subtract(-1, 1), -2)
         self.assertEqual(calc.subtract(-1, -1), 0)
 
 
 if __name__ == "__main__":
     unittest.main()
+
+
